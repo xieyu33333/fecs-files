1b2309afaf4f0a5c97659527a7d84e0f621f2feb
xieyu03 <xieyu03@baidu.com>
2015-02-03 23:25:30 +0800

    prevent interrupt watch

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 var fecs = require('fecs');
-var glob = require("glob")
+var glob = require("glob");
+var gulp = require("gulp");
+var through = require('through2');
+
+gulp.src('node_modules/fecs/cli/check.js')
+    .pipe(remove())
+    .pipe(gulp.dest('node_modules/fecs/cli/'));
+
 
 exports.check = function (path) {
    if (typeof(path) === 'string') {
@@ -8,7 +15,7 @@ exports.check = function (path) {
         })
    }
    else if (toString.call(path) === '[object Array]') {
-        var fileList = []
+        var fileList = [];
         for (var i = 0; i < path.length; i++) {
             glob(path[i], {}, function (er, files) {
                 fileList = fileList.concat(files);
@@ -39,27 +46,51 @@ function check(files) {
     fecs.check(fecs_opts);
 }
 
+
+function remove() {
+  // Creating a stream through which each file will pass
+    return through.obj(function(file, enc, cb) {
+        if (file.isNull()) {
+          // return empty file
+            cb(null, file);
+        }
+        if (file.isBuffer()) {
+            var newString = file.contents.toString().replace("process.exit(success ? 0 : 1);", "");
+            file.contents = new Buffer(newString);
+        }
+        if (file.isStream()) {
+            file.contents = file.contents.pipe(prefixStream(prefixText));
+        }
+
+        cb(null, file);
+
+    });
+};
 // exports.format = function (opts) {
 //     var opts = opts || {};
 //     var output = opts.output || './output';
 
-//     var fecs_opts = { 
-//         _: dir(path),
-//         color: true,
-//         debug: false,
-//         help: false,
-//         lookup: true,
-//         rule: false,
-//         silent: false,
-//         sort: false,
-//         stream: false,
-//         version: false,
-//         reporter: 'baidu',
-//         format: '',
-//         output: './output',
-//     }
+//     { _: 
+//        [ 'src/plugins/islider_animate.js',
+//          'src/plugins/islider_button.js',
+//          'src/plugins/islider_dot.js',
+//          'src/plugins/islider_zoom.js' ],
+//       color: true,
+//       debug: false,
+//       help: false,
+//       lookup: true,
+//       rule: false,
+//       silent: false,
+//       sort: false,
+//       stream: false,
+//       r: false,
+//       version: false,
+//       format: '',
+//       output: './output',
+//       reporter: '' 
+//   }
     
-//     fecs.check(fecs_opts);
+//     fecs.format(fecs_opts);
 // }   
 
 
